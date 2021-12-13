dbc299c02a38a57dada34ad7fef9c276f75682cb
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2021-12-13 19:01:01 +0530

    base model

---
+++ TeamSeas.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Scraping Team Seas Data for analysis
+
+# In[1]:
+
+
+import requests
+import pandas
+from bs4 import BeautifulSoup
+from datetime import datetime
+import locale
+# import matplotlib.pyplot as plt
+
+def get_count():
+    req = requests.get("https://tscache.com/donation_total.json")
+    return req.json()["count"]
+
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+headers = {"User-Agent": "Mozilla/5.0"}
+# r = requests.get("https://teamseas.org/all-donors/", headers=headers)
+r = requests.get("https://tscache.com/lb_recent.json")
+# soup = BeautifulSoup(r.content, "html.parser")
+donations = r.json()
+# donations.keys()
+# donations["recent"]
+
+df_li = []
+for recents in donations["recent"]:
+    d = {}
+    d["name"] = recents["name"]
+    if recents["team_name"]:
+        d["team_name"] = recents["team_name"]
+    else:
+        d["team_name"] = "Anonymous"
+    d["created"] = datetime.strptime(datetime.utcfromtimestamp(int(recents["created_at"])).strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+    d["amount"] = locale.atoi(recents["pounds"])
+    df_li.append(d)
+
+df = pandas.DataFrame(df_li)
+
+# ## Plot of Donations vs Time
+
+
+def donation_time():
+    return df
+# plt.figure(figsize=(25, 10))
+# # plt.yticks(range(min(df["amount"]), max(df["amount"])))
+# plt.ylim(0, df["amount"].max())
+# plt.plot(df["created"], df["amount"])
+
+
+# ## Grouping according to teams
+
+def team_group():
+    grouped_data = df.groupby(df["team_name"])["amount"].sum()
+
+    teams_df = pandas.DataFrame({"team_name": grouped_data.index, "total_donated": grouped_data.values})
+
+    teams_df.sort_values(by=["total_donated"], ascending=False, inplace=True)
+    return teams_df
+
+# plt.figure(figsize=(25, 3))
+# plt.pie(teams_df["total_donated"], labels=teams_df["team_name"])
+
+
+# ## Hourly Average
+def hour_group():
+    df["hour"] = df["created"].dt.strftime("%H")
+
+    hourly_grp = df.groupby(df["hour"])["amount"].sum()
+
+    hourly_df = pandas.DataFrame({"hour": hourly_grp.index, "total_donation": hourly_grp.values})
+    return hourly_df
+
+# plt.figure(figsize=(25, 3))
+# plt.plot(hourly_df["hour"], hourly_df["total_donation"])
+
+
+# ## Minute Average 
+def minute_group():
+    df["minute"] = df["created"].dt.strftime("%H-%M")
+
+    minute_grp = df.groupby(df["minute"])["amount"].sum()
+
+    minute_df = pandas.DataFrame({"minute": minute_grp.index, "total_donation": minute_grp.values})
+    return minute_df
+
+# plt.figure(figsize=(30, 3))
+# plt.plot(minute_df["minute"], minute_df["total_donation"])
+
+df_li2 = []
+for mst in donations["most"]:
+    d = {}
+    d["name"] = mst["name"]
+    d["amount"] = locale.atoi(mst["pounds"])
+    d["created"] = datetime.strptime(datetime.utcfromtimestamp(int(mst["created_at"])).strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+    df_li2.append(d)
+
+df2 = pandas.DataFrame(df_li2)
+
+# plt.figure(figsize=(25, 10))
+# plt.bar(df["name"], df["amount"], width=0.4)
+
+
+def single_most_donations():
+    return df2
+
+
+# plt.figure(figsize=(20,3))
+# plt.plot(date_sorted["created"], date_sorted["amount"])
+
+
+def date_sorted():
+    sorted_df = df2.sort_values(by=["created"])
+    return sorted_df
+
+
+df_li3 = []
+for i in donations["teams_most_donations"]:
+    d = {}
+    if not i["team"]:
+        d["team_name"] = "Anonymous"
+    else:
+        d["team_name"] = i["team"]
+    d["donation"] = locale.atoi(i["total_donation"])
+    d["members"] = int(i["total_members"])
+    df_li3.append(d)
+
+df3 = pandas.DataFrame(df_li3)
+df3["avg"] = df3["donation"]/df3["members"]
+
+# plt.figure(figsize=(25, 3))
+# plt.plot(df["team_name"], df["donation"])
+
+
+def team_donation():
+    return df3
+
+# plt.figure(figsize=(25, 3))
+# plt.plot(df["team_name"], df["avg"])
+
---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template
+import TeamSeas
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    data = {}
+
+    data["count"] = TeamSeas.get_count()
+
+    overall_df = TeamSeas.donation_time()
+    team_grp = TeamSeas.team_group()
+    hour_grp = TeamSeas.hour_group()
+    minute_grp = TeamSeas.minute_group()
+
+    df_labels = [i.strftime("%Y-%m-%d %H:%M:%S") for i in overall_df["created"]]
+    df_values = [i for i in overall_df["amount"]]
+    data["graph1"] = {"label": df_labels, "value": df_values}
+
+    team_labels = [i for i in team_grp["team_name"].values]
+    team_values = [i for i in team_grp["total_donated"].values]
+    data["graph2"] = {"label": team_labels, "value": team_values}
+
+    hour_labels = [i for i in hour_grp["hour"]]
+    hour_values = [i for i in hour_grp["total_donation"]]
+    data["graph3"] = {"label": hour_labels, "value": hour_values}
+
+    minute_labels = [i for i in minute_grp["minute"]]
+    minute_values = [i for i in minute_grp["total_donation"]]
+    data["graph4"] = {"label": minute_labels, "value": minute_values}
+
+    return render_template("index.html", data=data)
+
+
+# data -> donation vs time -> label value
+#      -> team vs donation -> label value | pie chart
+#      -> hourly average -> label value
+#      -> minute average -> label value
+
+@app.route("/most")
+def teams():
+    data = {}
+
+    data["count"] = TeamSeas.get_count()
+
+    overall_df = TeamSeas.single_most_donations()
+    overall_labels = [i for i in overall_df["name"]]
+    overall_values = [i for i in overall_df["amount"]]
+    data["graph1"] = {"label": overall_labels, "value": overall_values}
+
+    sorted_df = TeamSeas.date_sorted()
+    date_labels = [i.strftime("%Y-%m-%d %H:%M:%S") for i in sorted_df["created"]]
+    date_values = [i for i in sorted_df["amount"]]
+    date_name = [i for i in sorted_df["name"]]
+    data["graph2"] = {"label": date_labels, "value": date_values, "name": date_name}
+
+    team_donation = TeamSeas.team_donation()
+    team_labels = [i for i in team_donation["team_name"]]
+    team_values = [i for i in team_donation["donation"]]
+    avg_value = [i for i in team_donation["avg"]]
+    data["graph3"] = {"label": team_labels, "value1": team_values, "value2": avg_value}
+    return render_template("teams.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
